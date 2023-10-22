f06102afec301a7b37483e0b51a859fe34e1187f
Sreekanth Cherukuri <scherukuri2003@gmail.com>
2023-10-22 18:22:29 -0400

    Test deployment

--- src/server/main.ts
+++ src/server/main.ts
@@ -3,10 +3,6 @@ import ViteExpress from "vite-express";
 
 const app = express();
 
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + React + TypeScript!");
-});
-
-ViteExpress.listen(app, 3000, () =>
+ViteExpress.listen(app, 3000, () => {
   console.log("Server is listening on port 3000...")
-);
+});
